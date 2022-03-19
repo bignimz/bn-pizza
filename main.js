d725708296a1c6b6288713c160e9d1bc05e6fea9
[alroude] <[nimrod.chitayi@gmail.com]>
2022-03-20 01:51:16 +0300

    created function to display selected pizza and calculate its value when place order button is clicked

--- main.js
+++ main.js
@@ -1,31 +1,76 @@
 'use strict';
 
-// const order = document.getElementById('order');
-// const results = document.getElementById('result');
-// order.addEventListener('click', function(){
-//     results.classList.remove('hidden');
-// })
 
-let totalCounter = $('.qty');
-let count = 0;
-
-let totals = Number($('#totals').val());
-
-function increment(){
-    count++;
-    $(totalCounter).html(count);
-}
-
-function decrement(){
-    count--;
-    $(totalCounter).html(function(){
-        return count * 1 <= 1 ? 1 : count * 1 - 1;
-    });
-}
-
-$(document).ready(function(){
+        // When Place Order button is clicked
     $('.submit').click(function(){
-        $('.results').slideDown(1000);
+
+        let pizzaSize = $(".pizza-size option:selected").val();
+        let pizzaCrust = $(".crust option:selected").val();
+        let pizzaToppings = $(".toppings option:selected").val();
+        let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        let order = 1;
+        let grandTotal = 0;
+
+        $('.submit').hide();
+        $('#addMore').show();
+        $('.results').slideDown(500);
+
+        $("#size").html($(".pizza-size option:selected").text() + " @ " + pizzaSize);
+        $("#crust").html($(".crust option:selected").text() + " @ " + pizzaCrust);
+        $("#toppings").html("With " + $(".toppings option:selected").text() + " @ " + pizzaToppings);
+        $("#totals").html("Ksh" + pizzaValue());
+        $('.g-total').html(`Total Bill: ${grandTotal + total}`);
     })
-})
+
+    // When Done Shopping button is clicked
+    $('.done').click(function(){
+        $('.done').toggle('hide');
+        $('.address').slideDown();
+    })
+
+    // When ADD Address button is clicked
+    $('#addressAdd').click(function(){
+        let address = $('#address').val();
+        $('addressAdd').toggle('hide');
+        $('.address').toggle('hide');
+        $('.address-results').toggle('show');
+        $('.location').text(address);
+        $('.checkout-btn').toggle('show');
+    })
+
+    const pizzaValue = function(){
+        let pizzaSize = $(".pizza-size option:selected").val();
+        let pizzaCrust = $(".crust option:selected").val();
+        let pizzaToppings = $(".toppings option:selected").val();
+        let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        return total;
+    }
+
+    let totalCounter = $('.qty');
+    let count = 1;
+
+    // Increase Quantity
+    function increment(){
+        count++;
+        $(totalCounter).html(count);
+        $("#totals").html(pizzaValue() * count);
+        $(".g-total").html(pizzaValue() + grandTotal);
+    }
+
+    // Decrease Quantity
+    function decrement(){
+        
+        $(totalCounter).html(function(){
+            count--;
+            $(totalCounter).html(count);
+            $("#totals").html(pizzaValue() * count -1);
+            if(count * 1 <= 1){
+                return 1; 
+            }else {return count * 1 - 1} 
+        });
+    }
+
+
+
+
 

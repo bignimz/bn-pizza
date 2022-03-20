da48ede652bd67b3c70a4fefd974ffccd3439c03
[alroude] <[nimrod.chitayi@gmail.com]>
2022-03-20 21:24:23 +0300

    Added functionality to the checkout button to display the modal window with the order details

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
         // When Place Order button is clicked
-    $('.submit').click(function(){
+    $('#order').click(function(){
 
         let pizzaSize = $(".pizza-size option:selected").val();
         let pizzaCrust = $(".crust option:selected").val();
@@ -20,23 +20,96 @@
         $("#toppings").html("With " + $(".toppings option:selected").text() + " @ " + pizzaToppings);
         $("#totals").html("Ksh" + pizzaValue());
         $('.g-total').html(`Total Bill: ${grandTotal + total}`);
+
+        let totalCounter = $('.qty');
+        let count = 1;
+
+        // Increase Quantity
+        function increment(){
+            count++;
+            $(totalCounter).html(count);
+            $("#totals").html(total * count);
+            // $(".g-total").html(grandTotal += total);
+        }
+
+        // Decrease Quantity
+        function decrement(){
+            
+            $(totalCounter).html(function(){
+                count--;
+                $(totalCounter).html(count);
+                $("#totals").html(pizzaValue() * count);
+                if(count * 1 <= 1){
+                    return 1; 
+                }else {return count * 1 - 1} 
+            });
+        } 
+
+         // When Done Shopping button is clicked
+         $('.done').click(function(){
+            $('.done').toggle('hide');
+            $('.address').slideDown();
+        })
+    
+        // When ADD Address button is clicked
+        $('#addressAdd').click(function(){
+            let address = $('#address').val();
+            $('addressAdd').toggle('hide');
+            $('.address').toggle('hide');
+            $('.address-results').toggle('show');
+            $('.location').text(address);
+            $(".g-total").html(`Total Bill: ${grandTotal + total + 200}`);
+            $('.checkout-btn').toggle('show');
+        })
+    
+        // When Checkout button is clicked
+        $('#checkout').click(function(){
+            $('.overlay').show();
+            $('#checkoutForm').toggle('show');
+            $('.del-location').text($('#address').val());
+            $('.del-bill').text(`${grandTotal + total + 200}`);
+            console.log($('#address').val());
+        })
+
+        // When Ok button on the modal is clicked
+        $('#ok').click(function(){
+            $('.overlay').toggle('hide');
+            $('#checkoutForm').toggle('hide');
+            $('#checkout').hide();
+            $('.address-results').hide();
+            $('.g-total').hide();
+        })
+
+         // When overlay is clicked
+        $('.overlay').click(function(){
+            $('.overlay').toggle('hide');
+            $('#checkoutForm').toggle('hide');
+        })
     })
 
-    // When Done Shopping button is clicked
-    $('.done').click(function(){
-        $('.done').toggle('hide');
-        $('.address').slideDown();
+    // When Order more button is clicked
+    $('#addMore').click(function(){
+
+        let pizzaSize = $(".pizza-size option:selected").val();
+        let pizzaCrust = $(".crust option:selected").val();
+        let pizzaToppings = $(".toppings option:selected").val();
+        let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        let order = 1;
+        let grandTotal = 0;
+
+        $('.submit').hide();
+        $('#addMore').show();
+        $('.results').slideDown(500);
+
+        $("#size").html($(".pizza-size option:selected").text() + " @ " + pizzaSize);
+        $("#crust").html($(".crust option:selected").text() + " @ " + pizzaCrust);
+        $("#toppings").html("With " + $(".toppings option:selected").text() + " @ " + pizzaToppings);
+
+        $("#totals").html("Ksh" + total);
+        $('.g-total').html(`Total Bill: ${grandTotal + total}`);
+
     })
 
-    // When ADD Address button is clicked
-    $('#addressAdd').click(function(){
-        let address = $('#address').val();
-        $('addressAdd').toggle('hide');
-        $('.address').toggle('hide');
-        $('.address-results').toggle('show');
-        $('.location').text(address);
-        $('.checkout-btn').toggle('show');
-    })
 
     const pizzaValue = function(){
         let pizzaSize = $(".pizza-size option:selected").val();
@@ -46,31 +119,4 @@
         return total;
     }
 
-    let totalCounter = $('.qty');
-    let count = 1;
-
-    // Increase Quantity
-    function increment(){
-        count++;
-        $(totalCounter).html(count);
-        $("#totals").html(pizzaValue() * count);
-        $(".g-total").html(pizzaValue() + grandTotal);
-    }
-
-    // Decrease Quantity
-    function decrement(){
-        
-        $(totalCounter).html(function(){
-            count--;
-            $(totalCounter).html(count);
-            $("#totals").html(pizzaValue() * count -1);
-            if(count * 1 <= 1){
-                return 1; 
-            }else {return count * 1 - 1} 
-        });
-    }
-
-
-
-
-
+    

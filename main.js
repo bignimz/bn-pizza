d3ad80fd47fe571a048f1015aca684f624d53af2
[alroude] <[nimrod.chitayi@gmail.com]>
2022-03-21 09:42:54 +0300

    Added a pizza constructor

--- main.js
+++ main.js
@@ -21,29 +21,7 @@
         $("#totals").html("Ksh" + pizzaValue());
         $('.g-total').html(`Total Bill: ${grandTotal + total}`);
 
-        let totalCounter = $('.qty');
-        let count = 1;
-
-        // Increase Quantity
-        function increment(){
-            count++;
-            $(totalCounter).html(count);
-            $("#totals").html(total * count);
-            // $(".g-total").html(grandTotal += total);
-        }
-
-        // Decrease Quantity
-        function decrement(){
-            
-            $(totalCounter).html(function(){
-                count--;
-                $(totalCounter).html(count);
-                $("#totals").html(pizzaValue() * count);
-                if(count * 1 <= 1){
-                    return 1; 
-                }else {return count * 1 - 1} 
-            });
-        } 
+        
 
          // When Done Shopping button is clicked
          $('.done').click(function(){
@@ -66,9 +44,10 @@
         $('#checkout').click(function(){
             $('.overlay').show();
             $('#checkoutForm').toggle('show');
+            $('.details').html("Pizza " + $(".pizza-size option:selected").text() + ", " + $(".crust option:selected").text() + " with " + $(".toppings option:selected").text());
             $('.del-location').text($('#address').val());
             $('.del-bill').text(`${grandTotal + total + 200}`);
-            console.log($('#address').val());
+            console.log($(pizzaSize).html());
         })
 
         // When Ok button on the modal is clicked
@@ -87,6 +66,8 @@
         })
     })
 
+       
+
     // When Order more button is clicked
     $('#addMore').click(function(){
 
@@ -94,7 +75,7 @@
         let pizzaCrust = $(".crust option:selected").val();
         let pizzaToppings = $(".toppings option:selected").val();
         let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
-        let order = 1;
+        order = order + 1;
         let grandTotal = 0;
 
         $('.submit').hide();
@@ -108,6 +89,21 @@
         $("#totals").html("Ksh" + total);
         $('.g-total').html(`Total Bill: ${grandTotal + total}`);
 
+        // Pizza Constructor Function
+        function Pizza(orderNo, size, crust, toppings, total) {
+            this.orderNo = orderNo;
+            this.size = size;
+            this.toppings = toppings;
+            this.crust = crust;
+            this.total = total;
+          }
+
+        let orderNew = new Pizza(order, pizzaSize, pizzaCrust, pizzaToppings, total);
+
+        let newRow = '<tr><th scope="row">' + orderNew.orderNo  + '</th><td id="size">' + $(pizzaSize).text() + " - " + orderNew.size + '</td><td id="crust">' + $(pizzaCrust).text() + " - " + orderNew.pizzaCrust + '</td><td id="toppings">' + $(pizzaToppings).text() + " - " + orderNew.pizzaToppings + '</td><td id="total">' + orderNew.total + '</td></tr>'
+
+        $(".resultsRow").append(newRow);
+
     })
 
 
@@ -118,5 +114,35 @@
         let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
         return total;
     }
+}
+
+let totalCounter = $('.qty');
+let count = 1;
+
+// Increase Quantity
+function increment(){
+    count++;
+    let grandTotal = 0;
+    grandTotal += pizzaValue();
+    $(totalCounter).html(count);
+    $("#totals").html(pizzaValue() * count);
+    $(".g-total").text(grandTotal += pizzaValue());
+    
+}
+
+// Decrease Quantity
+function decrement(){
+    
+    $(totalCounter).html(function(){
+        count--;
+        $(totalCounter).html(count);
+        $("#totals").html(pizzaValue() * count);
+        if(count * 1 <= 1){
+            return 1; 
+        }else {return count * 1 - 1} 
+    });
+
+    
+// orderPizza();
 
     
